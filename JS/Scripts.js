6c978ca49dedcb972b9774ffba7651cbdc8f6487
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-10-31 11:11:17 -0700

    Test 3 pass

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -2,13 +2,20 @@
 const BankAccount = {
   createAccount: function (name, initialDeposit) {
     return {
-      name: name,
+      name: fullName,
       balance: initialDeposit,
       deposit: function (amount) {
         if (amount > 0) {
           this.balance += amount;
         } else {
           console.log('Deposit amount must be greater than 0.');
+        }
+      },
+      withdraw: function (amount) {
+        if (amount > 0 && amount <= this.balance) {
+          this.balance -= amount;
+        } else {
+          console.log('Withdrawal amount must be greater than 0 and not exceed the account balance.');
         }
       },
     };
